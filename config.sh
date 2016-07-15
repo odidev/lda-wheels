ef0da6fec597d502acf1f40dced3084eed297a54
ariddell <ariddell@users.noreply.github.com>
2016-07-14 21:46:38 -0400

    Customize for lda (rather than matplotlib)

--- config.sh
+++ config.sh
@@ -9,44 +9,23 @@ function pre_build {
         export CXX=clang++
         brew install pkg-config
     fi
-    source multibuild/library_builders.sh
-    build_jpeg
-    build_libpng
-    build_bzip2
-    # Use local freetype for versions which support it
-    if [[ "$BUILD_COMMIT" =~ v2\. ]]; then
-        export MPLLOCALFREETYPE=1
-    else
-        build_freetype
-    fi
 }
 
 function build_wheel {
     # Override common_utils build_wheel function to fix version error
-    build_bdist_wheel $@
+    # pbr is easily confused about what git repository it is in, provide version
+    PBR_VERSION=${BUILD_COMMIT} build_bdist_wheel $@
 }
 
 function run_tests {
-    # Runs tests on installed distribution from an empty directory
-    MPL_SRC_DIR=../matplotlib
-    # Get test images
-    MPL_INSTALL_DIR=$(dirname $(python -c 'import matplotlib; print(matplotlib.__file__)'))
-    cp -r ${MPL_SRC_DIR}/lib/matplotlib/tests/baseline_images $MPL_INSTALL_DIR/tests
-
+    SRC_DIR=../lda
     echo "sanity checks"
-    python -c "import dateutil; print(dateutil.__version__)"
     python -c "import sys; print('\n'.join(sys.path))"
-    python -c "import matplotlib; print(matplotlib.__file__)"
-    python -c "from matplotlib import font_manager"
+    python -c "import lda"
+    pip install -r $SRC_DIR/requirements.txt
+    pip install -r $SRC_DIR/test-requirements.txt
+    pip install nose
 
-    echo "testing matplotlib using 1 process"
-    # 1.5.x has pesky unicode error for sphinx extension test
-    local mpl_version=$(python -c "import matplotlib; print(matplotlib.__version__)")
-    if [[ "$mpl_version" =~ 1\. ]]; then
-        local extra_test_args="-e TestTinyPages"
-    fi
-    python $MPL_SRC_DIR/tests.py -sv $extra_test_args
-
-    echo "Check import of tcl / tk"
-    MPLBACKEND="tkagg" python -c 'import matplotlib.pyplot as plt; print(plt.get_backend())'
+    echo "testing"
+    nosetests lda
 }
